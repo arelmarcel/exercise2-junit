ae89f73f61edef13a3021cb773c50e2281d7c23c
arel <marcellinus.arel@mitrais.com>
2022-03-02 13:18:49 +0700

    add more tests

--- src/test/java/com/mitrais/TransactionTest.java
+++ src/test/java/com/mitrais/TransactionTest.java
@@ -31,9 +31,18 @@ class TransactionTest {
 
         transaction.SendPoint(usersList.get(0), usersList.get(1), 100, 12, 1,2020);
 
+        List<HistoryData> historyLists =  transaction.getHistory();
+
+
         assertAll(
                 () -> assertEquals(1900,  arel.getPoint() ),
-                () -> assertNotEquals(2000, bernard.getPoint())
+                () -> assertNotEquals(2000, bernard.getPoint()),
+
+                () -> assertEquals("Transaction success", historyLists.get(0).status),
+                () -> assertNotEquals("Transaction fail (doesn't have enough point)", historyLists.get(0).status),
+
+                () -> assertNotNull(historyLists)
+
         );
     }
 }
